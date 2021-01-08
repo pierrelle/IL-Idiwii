975d4b7706bee9c1ecc47cdf7038168d58ee899e
Pierrick Croissant <pcroissant@ensc.fr>
2021-01-08 12:06:24 +0100

    feat: add locust logger

--- test/LocustTest.py
+++ test/LocustTest.py
@@ -1,7 +1,10 @@
 import time
+import logging
+
 from locust import HttpUser, task
 
 class QuickstartUser(HttpUser):
     @task
     def test_intent_route(self):
-        self.client.get("/api/intent/sentence=hello world")
+        response = self.client.get("/api/intent?sentence=test")
+        logging.info(f"Response status code: {response.status_code}")
